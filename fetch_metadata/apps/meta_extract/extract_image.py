40947fc6ebee59ad1b4eabaccc5a3013e046d5cc
Thobiy <tobiemmanuel19@gmail.com>
2022-07-26 03:01:19 +0100

    first push to meta_extract app

---
+++ fetch_metadata/apps/meta_extract/extract_image.py
@@ -0,0 +1,33 @@
+# pip install pillow
+# Run python extract_image.py
+#
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+imagename = "AMS_2882.nef"
+
+image = Image.open(imagename)
+
+info_dict = {
+    "Filename": image.filename,
+    "Image Size": image.size,
+    "Image Height": image.height,
+    "Image Width": image.width,
+    "Image Format": image.format,
+    "Image Mode": image.mode,
+    "Image is Animated": getattr(image, "is_animated", False),
+    "Frames in Image": getattr(image, "n_frames", 1)
+}
+
+for label, value in info_dict.items():
+    print(f"{label:25}: {value}")
+exifdata = image.getexif()
+
+for tag_id in exifdata:
+    tag = TAGS.get(tag_id, tag_id)
+    data = exifdata.get(tag_id)
+
+    if isinstance(data, bytes):
+        data = data.decode()
+    print(f"{tag:25}: {data}")
